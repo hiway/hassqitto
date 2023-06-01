d4746115c6e701d8dde513167c4dd37d90e15e87
Harshad Sharma <harshad@sharma.io>
2023-06-01 15:38:25 +0530

    Add comments, remove if __name__...

--- examples/button_entity.py
+++ examples/button_entity.py
@@ -2,51 +2,64 @@ import time
 import hassquitto as hq
 import logging
 
+# Enable logging
 logging.getLogger("hassquitto").setLevel(logging.DEBUG)
 
+# Define message.
+MESSAGE = "Hello, World!"
 
+
+# Define the device.
 class Device(hq.Device):
     greet = hq.Button(name="Greet")
     message = hq.Text(name="Message")
 
 
+# Instantiate the device.
 device = Device(name="Example Button Entity")
-MESSAGE = "Hello, World!"
 
 
+# Handler for button click event.
 @device.greet.on_click
 def greet_on_click(_state):
-    global MESSAGE
-
     print("Greet button clicked!")
 
+    # Publish message.
+    global MESSAGE
     device.message.publish_state(MESSAGE)
 
-    # Toggle the message.
+    # Toggle message.
     MESSAGE = "Hello, World!" if MESSAGE == "Goodbye, World!" else "Goodbye, World!"
 
 
-if __name__ == "__main__":
-    # To see the device in Home Assistant, open the following URL:
-    # http://homeassistant.local:8123/config/devices/dashboard
-    # look for "Example Button Entity" and click on the name.
+# To see the device in Home Assistant, open the following URL:
+# http://homeassistant.local:8123/config/devices/dashboard
+# look for "Example Button Entity" and click on the name.
 
-    # Refresh the Device page if all entities don't show up.
+# Refresh the Device page if all entities don't show up.
 
-    try:
-        # Connect to MQTT.
-        device.connect(
-            host="homeassistant.local",
-            port=1883,  # MQTT port.
-            username="example",  # MQTT username.
-            password="example",  # MQTT password.
-        )
-        device.set_available()
-        device.set_online()
+try:
+    # Connect to MQTT.
+    device.connect(
+        host="homeassistant.local",
+        port=1883,  # MQTT port.
+        username="example",  # MQTT username.
+        password="example",  # MQTT password.
+    )
+    # Set device as available and online.
+    device.set_available()
+    device.set_online()
 
-        device.run()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        device.destroy_discovery()
-        device.disconnect()
+    # Loop forever.
+    device.run()
+
+except KeyboardInterrupt:
+    # Wait for Ctrl+C.
+    pass
+
+finally:
+    # Remove example device from Home Assistant.
+    device.destroy_discovery()
+
+    # Disconnect from MQTT.
+    device.disconnect()
