2e71821e5b84349f200edb9444eab1af4dc0e363
Harshad Sharma <harshad@sharma.io>
2023-06-01 12:56:23 +0530

    Add logging

---
+++ src/hassquitto/logging.py
@@ -0,0 +1,32 @@
+"""
+Logging
+"""
+import logging as _logging
+
+
+def get_logger(name: str) -> _logging.Logger:
+    """
+    Get a logger with the given name.
+
+    Args:
+        name: Name of the logger.
+
+    Returns:
+        Logger.
+
+    Logging to stdout is disabled by default.
+    To enable logging, set the level.
+
+        import logging
+        logging.getLogger("hassquitto").setLevel(logging.DEBUG)
+    """
+    logger = _logging.getLogger(name)
+    formatter = _logging.Formatter(
+        "[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S %z",
+    )
+    handler = _logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(_logging.NOTSET)
+    return logger
