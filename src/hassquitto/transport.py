b53c668544fd75e7cca3d84c6fa1bf1c74e4ba61
Harshad Sharma <harshad@sharma.io>
2023-06-02 18:53:23 +0530

    Add async mqtt transport

---
+++ src/hassquitto/transport.py
@@ -0,0 +1,112 @@
+"""
+Async MQTT Transport
+"""
+import asyncio
+
+from asgiref.sync import sync_to_async
+from paho.mqtt import client as mqtt
+
+
+class AsyncMQTT:
+    """
+    Async MQTT Transport
+    """
+
+    def __init__(self) -> None:
+        self._client = mqtt.Client()
+        self.loop = None
+
+    def ensure_loop(self) -> None:
+        """
+        Ensure that the transport has a loop
+        """
+        if self.loop is None:
+            self.loop = asyncio.get_event_loop()
+
+    def on_connect(self, coro) -> None:
+        """
+        Decorator for on_connect callback
+        """
+
+        def wrapper(client: mqtt.Client, userdata: None, flags: dict, rc: int) -> None:
+            asyncio.run_coroutine_threadsafe(
+                coro(client, userdata, flags, rc), self.loop
+            )
+
+        self._client.on_connect = wrapper
+        return wrapper
+
+    def on_disconnect(self, coro) -> None:
+        """
+        Decorator for on_disconnect callback
+        """
+
+        def wrapper(client: mqtt.Client, userdata: None, rc: int) -> None:
+            asyncio.run_coroutine_threadsafe(coro(client, userdata, rc), self.loop)
+
+        self._client.on_disconnect = wrapper
+        return wrapper
+
+    def on_message(self, coro) -> None:
+        """
+        Decorator for on_message callback
+        """
+
+        def wrapper(client: mqtt.Client, userdata: None, msg: mqtt.MQTTMessage) -> None:
+            asyncio.run_coroutine_threadsafe(coro(client, userdata, msg), self.loop)
+
+        self._client.on_message = wrapper
+        return wrapper
+
+    async def connect(self, host: str, port: int, username: str, password: str) -> None:
+        """
+        Connect to MQTT broker
+        """
+        self.ensure_loop()
+        await sync_to_async(self._client.username_pw_set)(username, password)
+        await sync_to_async(self._client.connect)(host, port)
+        await sync_to_async(self._client.loop_start)()
+
+    def sync_connect(self, host: str, port: int, username: str, password: str) -> None:
+        """
+        Connect to MQTT broker
+        """
+        self._client.username_pw_set(username, password)
+        self._client.connect(host, port)
+        self._client.loop_start()
+
+    async def disconnect(self) -> None:
+        """
+        Disconnect from MQTT broker
+        """
+        self.ensure_loop()
+        await sync_to_async(self._client.loop_stop)()
+        await sync_to_async(self._client.disconnect)()
+
+    def sync_disconnect(self) -> None:
+        """
+        Disconnect from MQTT broker
+        """
+        self._client.loop_stop()
+        self._client.disconnect()
+
+    async def publish(self, topic: str, payload: str) -> None:
+        """
+        Publish message to MQTT broker
+        """
+        self.ensure_loop()
+        await sync_to_async(self._client.publish)(topic, payload)
+
+    async def subscribe(self, topic: str) -> None:
+        """
+        Subscribe to topic on MQTT broker
+        """
+        self.ensure_loop()
+        await sync_to_async(self._client.subscribe)(topic)
+
+    async def unsubscribe(self, topic: str) -> None:
+        """
+        Unsubscribe from topic on MQTT broker
+        """
+        self.ensure_loop()
+        await sync_to_async(self._client.unsubscribe)(topic)
---
+++ tests/test_transport.py
@@ -0,0 +1,108 @@
+import asyncio
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+from hassquitto.transport import AsyncMQTT
+
+
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+from hassquitto.transport import AsyncMQTT
+
+
+@pytest.fixture
+async def mqtt():
+    mqtt = AsyncMQTT()
+    mqtt._client.username_pw_set = MagicMock()
+    mqtt._client.connect = MagicMock()
+    mqtt._client.disconnect = MagicMock()
+    mqtt._client.loop_start = MagicMock()
+    mqtt._client.publish = MagicMock()
+    mqtt._client.subscribe = MagicMock()
+    mqtt._client.unsubscribe = MagicMock()
+    mqtt._client.loop_stop = MagicMock()
+    mqtt.loop = asyncio.get_event_loop()
+    yield mqtt
+
+
+async def test_publish(mqtt):
+    topic = "test/topic"
+    payload = "test payload"
+    await mqtt.publish(topic, payload)
+    mqtt._client.publish.assert_called_once_with(topic, payload)
+
+
+async def test_subscribe(mqtt):
+    topic = "test/topic"
+    await mqtt.subscribe(topic)
+    mqtt._client.subscribe.assert_called_once_with(topic)
+
+
+async def test_unsubscribe(mqtt):
+    topic = "test/topic"
+    await mqtt.unsubscribe(topic)
+    mqtt._client.unsubscribe.assert_called_once_with(topic)
+
+
+async def test_on_connect(mqtt):
+    coro = AsyncMock()
+    wrapped = mqtt.on_connect(coro)
+
+    rc = 0
+    flags = {"test_flag": True}
+    wrapped(None, None, flags, rc)
+
+    coro.assert_called_once_with(None, None, flags, rc)
+
+
+async def test_on_disconnect(mqtt):
+    coro = AsyncMock()
+    mqtt.on_disconnect(coro)
+    wrapped = mqtt.on_disconnect(coro)
+
+    rc = 0
+    wrapped(None, None, rc)
+
+    coro.assert_called_once_with(None, None, rc)
+
+
+async def test_on_message(mqtt):
+    coro = AsyncMock()
+    mqtt.on_message(coro)
+    wrapped = mqtt.on_message(coro)
+
+    msg = MagicMock()
+    wrapped(None, None, msg)
+
+    coro.assert_called_once_with(None, None, msg)
+
+
+async def test_connect(mqtt):
+    await mqtt.connect("localhost", 1883, "username", "password")
+    mqtt._client.username_pw_set.assert_called_once_with("username", "password")
+    mqtt._client.connect.assert_called_once_with("localhost", 1883)
+    mqtt._client.loop_start.assert_called_once()
+
+
+async def test_disconnect(mqtt):
+    await mqtt.disconnect()
+    mqtt._client.loop_stop.assert_called_once()
+    mqtt._client.disconnect.assert_called_once()
+
+
+async def test_ensure_loop(mqtt):
+    mqtt.loop = None
+    mqtt.ensure_loop()
+    assert mqtt.loop is not None
+
+
+def test_sync_connect(mqtt):
+    mqtt.sync_connect("localhost", 1883, "username", "password")
+    mqtt._client.username_pw_set.assert_called_once_with("username", "password")
+    mqtt._client.connect.assert_called_once_with("localhost", 1883)
+    mqtt._client.loop_start.assert_called_once()
+
+
+def test_sync_disconnect(mqtt):
+    mqtt.sync_disconnect()
+    mqtt._client.loop_stop.assert_called_once()
+    mqtt._client.disconnect.assert_called_once()
