89a823761f577f5727e15ca9ac37f25f73e7398a
Harshad Sharma <harshad@sharma.io>
2023-06-02 19:11:27 +0530

    Fix tests for symbols

--- tests/test_symbols.py
+++ tests/test_symbols.py
@@ -1,9 +1,17 @@
 from hassquitto import symbols
 
 
-def test_binary_state_off():
-    assert symbols.BinaryState.OFF.value == "OFF"
+def test_device_availability_offline():
+    assert symbols.DeviceAvailability.OFFLINE.value == "offline"
 
 
-def test_binary_state_on():
-    assert symbols.BinaryState.ON.value == "ON"
+def test_device_availability_online():
+    assert symbols.DeviceAvailability.ONLINE.value == "online"
+
+
+def test_switch_state_off():
+    assert symbols.SwitchState.OFF.value == "OFF"
+
+
+def test_switch_state_on():
+    assert symbols.SwitchState.ON.value == "ON"
