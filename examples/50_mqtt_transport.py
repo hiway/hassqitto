b64582b85536e4f660fadbdc93d98a70cfbd60d0
Harshad Sharma <harshad@sharma.io>
2023-06-05 10:56:15 +0530

    Rename examples for easier cli access

--- examples/50_mqtt_transport.py
+++ examples/50_mqtt_transport.py
@@ -1,47 +1,48 @@
-import asyncio
 import logging
 import json
+import time
 
 from hassquitto.logging import get_logger
 from hassquitto.symbols import DeviceAvailability, SwitchState
-from hassquitto.transport import AsyncMQTT
+from hassquitto.transport import MqttTransport
 
 # Setup logging
 logger = get_logger(__name__)
 logger.setLevel(logging.DEBUG)
+logging.getLogger("hassquitto").setLevel(logging.DEBUG)
 
 # Instantiate MQTT transport
-transport = AsyncMQTT(client_id="transport-test")
+transport = MqttTransport(client_id="transport-test")
 
 
 # Define callback for MQTT connection
 @transport.on_connect
-async def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     logger.info("Connected with result code %s", rc)
 
 
 # Define callback for MQTT message
 @transport.on_message
-async def on_message(client, userdata, msg):
+def on_message(client, userdata, msg):
     logger.info("Received message on topic %s: %s", msg.topic, msg.payload.decode())
 
 
 # Define callback for MQTT disconnection
 @transport.on_disconnect
-async def on_disconnect(client, userdata, rc):
+def on_disconnect(client, userdata, rc):
     logger.info("Disconnected with result code %s", rc)
 
 
-async def main():
+def main():
     # Connect to MQTT broker
-    await transport.connect(
+    transport.connect(
         host="homeassistant.local",
         port=1883,
         username="example",
         password="example",
     )
     # Wait for connection to be established
-    await asyncio.sleep(1)
+    time.sleep(1)
 
     # Configure switch entity
     message = json.dumps(
@@ -62,18 +63,18 @@ async def main():
             },
         }
     )
-    await transport.publish("homeassistant/switch/transport_test/config", message)
+    transport.publish("homeassistant/switch/transport_test/config", message)
     logger.info("Configured device")
 
     # Subscribe to command topic for switch
-    await transport.subscribe("homeassistant/switch/transport_test/command")
+    transport.subscribe("homeassistant/switch/transport_test/command")
     logger.info("Subscribed to topic %s", "homeassistant/switch/transport_test/command")
 
     # Wait for configuration to be picked up by Home Assistant
-    await asyncio.sleep(0.5)
+    time.sleep(0.5)
 
     # Publish availability message
-    await transport.publish(
+    transport.publish(
         "homeassistant/switch/transport_test/availability",
         DeviceAvailability.ONLINE.value,
     )
@@ -81,31 +82,31 @@ async def main():
 
     # Publish switch state
     for i in range(3):
-        await transport.publish(
+        transport.publish(
             "homeassistant/switch/transport_test/state", SwitchState.ON.value
         )
         logger.info("Turned switch on.")
-        await asyncio.sleep(3)
+        time.sleep(3)
 
-        await transport.publish(
+        transport.publish(
             "homeassistant/switch/transport_test/state", SwitchState.OFF.value
         )
         logger.info("Turned switch off.")
-        await asyncio.sleep(3)
+        time.sleep(3)
 
     # Mark device as unavailable
-    await transport.publish(
+    transport.publish(
         "homeassistant/switch/transport_test/availability",
         DeviceAvailability.OFFLINE.value,
     )
     logger.info("Device is offline.")
 
     # Remove device from Home Assistant
-    await asyncio.sleep(1)
-    await transport.publish("homeassistant/switch/transport_test/config", "")
+    time.sleep(1)
+    transport.publish("homeassistant/switch/transport_test/config", "")
     logger.info("Removed device from Home Assistant.")
 
 
 if __name__ == "__main__":
-    # Run main coroutine
-    asyncio.run(main())
+    # Run main function
+    main()
--- examples/50_mqtt_transport_async.py
+++ examples/50_mqtt_transport_async.py
@@ -1,47 +1,48 @@
+import asyncio
 import logging
 import json
-import time
 
 from hassquitto.logging import get_logger
 from hassquitto.symbols import DeviceAvailability, SwitchState
-from hassquitto.transport import AsyncMQTT
+from hassquitto.transport import MqttTransport
 
 # Setup logging
 logger = get_logger(__name__)
 logger.setLevel(logging.DEBUG)
+logging.getLogger("hassquitto").setLevel(logging.DEBUG)
 
 # Instantiate MQTT transport
-transport = AsyncMQTT(client_id="transport-test")
+transport = MqttTransport(client_id="transport-test")
 
 
 # Define callback for MQTT connection
 @transport.on_connect
-def on_connect(client, userdata, flags, rc):
+async def on_connect(client, userdata, flags, rc):
     logger.info("Connected with result code %s", rc)
 
 
 # Define callback for MQTT message
 @transport.on_message
-def on_message(client, userdata, msg):
+async def on_message(client, userdata, msg):
     logger.info("Received message on topic %s: %s", msg.topic, msg.payload.decode())
 
 
 # Define callback for MQTT disconnection
 @transport.on_disconnect
-def on_disconnect(client, userdata, rc):
+async def on_disconnect(client, userdata, rc):
     logger.info("Disconnected with result code %s", rc)
 
 
-def main():
+async def main():
     # Connect to MQTT broker
-    transport.connect(
+    await transport.connect(
         host="homeassistant.local",
         port=1883,
         username="example",
         password="example",
     )
     # Wait for connection to be established
-    time.sleep(1)
+    await asyncio.sleep(1)
 
     # Configure switch entity
     message = json.dumps(
@@ -62,18 +63,18 @@ def main():
             },
         }
     )
-    transport.publish("homeassistant/switch/transport_test/config", message)
+    await transport.publish("homeassistant/switch/transport_test/config", message)
     logger.info("Configured device")
 
     # Subscribe to command topic for switch
-    transport.subscribe("homeassistant/switch/transport_test/command")
+    await transport.subscribe("homeassistant/switch/transport_test/command")
     logger.info("Subscribed to topic %s", "homeassistant/switch/transport_test/command")
 
     # Wait for configuration to be picked up by Home Assistant
-    time.sleep(0.5)
+    await asyncio.sleep(0.5)
 
     # Publish availability message
-    transport.publish(
+    await transport.publish(
         "homeassistant/switch/transport_test/availability",
         DeviceAvailability.ONLINE.value,
     )
@@ -81,31 +82,31 @@ def main():
 
     # Publish switch state
     for i in range(3):
-        transport.publish(
+        await transport.publish(
             "homeassistant/switch/transport_test/state", SwitchState.ON.value
         )
         logger.info("Turned switch on.")
-        time.sleep(3)
+        await asyncio.sleep(3)
 
-        transport.publish(
+        await transport.publish(
             "homeassistant/switch/transport_test/state", SwitchState.OFF.value
         )
         logger.info("Turned switch off.")
-        time.sleep(3)
+        await asyncio.sleep(3)
 
     # Mark device as unavailable
-    transport.publish(
+    await transport.publish(
         "homeassistant/switch/transport_test/availability",
         DeviceAvailability.OFFLINE.value,
     )
     logger.info("Device is offline.")
 
     # Remove device from Home Assistant
-    time.sleep(1)
-    transport.publish("homeassistant/switch/transport_test/config", "")
+    await asyncio.sleep(1)
+    await transport.publish("homeassistant/switch/transport_test/config", "")
     logger.info("Removed device from Home Assistant.")
 
 
 if __name__ == "__main__":
     # Run main coroutine
-    main()
+    asyncio.run(main())
